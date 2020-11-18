11da71c38898c889a41fb294883c3f1962769e54
joao <joao.camargo@gmail.com>
2020-11-18 20:35:34 -0300

    criada extensão

--- CleanApp/DataTests/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/RemoteAddAccountTests.swift
@@ -36,13 +36,15 @@ class RemoteAddAccountTests: XCTestCase {
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
+
+}
+
+extension RemoteAddAccountTests{
     class HttpClientSpy: HttpPostClient {
         var url: URL?
         
         func post(url: URL) {
             self.url = url
         }
-        
-        
     }
 }
