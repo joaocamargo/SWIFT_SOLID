33e26b82f67b4fc5f533b3c48c96bb8d720d8acc
joao <joao.camargo@gmail.com>
2020-11-21 11:42:34 -0300

    completion with error

---
+++ CleanApp/Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by joao camargo on 21/11/20.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ CleanApp/Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by joao camargo on 21/11/20.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
