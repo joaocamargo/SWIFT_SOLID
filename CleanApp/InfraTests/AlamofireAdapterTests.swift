5783e186172398b71ad336524523d3d8bd1e7892
joao <joao.camargo@gmail.com>
2020-11-30 19:33:50 -0300

    adicionado teste de method enviado

--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -16,7 +16,7 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL){
-        session.request(url).resume()
+        session.request(url,method: .post).resume()
     }
 }
 
@@ -31,6 +31,8 @@ class AlamofireAdapterTests: XCTestCase {
         let exp = expectation(description: "waiting")
             URLProtocolStub.observeRequest { (request) in
             XCTAssertEqual(url, request.url)
+            XCTAssertEqual("POST", request.httpMethod)
+
                 exp.fulfill()
             }
         wait(for: [exp], timeout: 1)
