ce5b4fccb967a55da7ffb36f484da57052b9e1da
joao <joao.camargo@gmail.com>
2020-11-30 19:32:24 -0300

    ensure alamo fire adapter calls alamo fire with correct url

--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -6,14 +6,62 @@
 //
 
 import XCTest
-@testable import Infra
+import Alamofire
+
+class AlamofireAdapter {
+    private let session: Session
+    
+    init(session: Session = .default){
+        self.session = session
+    }
+    
+    func post(to url: URL){
+        session.request(url).resume()
+    }
+}
 
 class AlamofireAdapterTests: XCTestCase {
     func test_() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let url = makeURL()
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: url)
+        let exp = expectation(description: "waiting")
+            URLProtocolStub.observeRequest { (request) in
+            XCTAssertEqual(url, request.url)
+                exp.fulfill()
+            }
+        wait(for: [exp], timeout: 1)
+        
     }
+    
+    
+    
+}
 
-
-
+class URLProtocolStub : URLProtocol {
+    
+    static var emit: ((URLRequest)-> Void)?
+    
+    static func observeRequest(completion: @escaping (URLRequest)->Void){
+        URLProtocolStub.emit = completion
+    }
+    
+    override open class func canInit(with request: URLRequest) -> Bool { //class func = estatico static
+        return true
+    }
+    
+    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override open func startLoading() {
+        URLProtocolStub.emit?(request)
+    }
+    
+    override open func stopLoading() {
+        
+    }
 }
