dbead635f13eaf69f381ba96c4f664b5685a2a86
joao <joao.camargo@gmail.com>
2020-12-05 11:25:49 -0300

    move exp to post completion handler

--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -54,7 +54,7 @@ class AlamofireAdapterTests: XCTestCase {
                 case .failure(let error): XCTAssertEqual(error, .noConnectivity)
             }
         }
-        exp.fulfill()        
+        exp.fulfill()
         wait(for: [exp],timeout: 1)
     }
 }
@@ -82,13 +82,14 @@ extension AlamofireAdapterTests {
     
     func testRequestFor(url: URL = makeURL(), data: Data?, action: @escaping(URLRequest) -> Void){
         let sut = makeSut()
-        sut.post(to: url, with: data) { _ in }
         let exp = expectation(description: "waiting")
-            URLProtocolStub.observeRequest { (request) in
-                action(request)
-                exp.fulfill()
-            }
+        sut.post(to: url, with: data) { _ in exp.fulfill()}
+        var request: URLRequest?
+        URLProtocolStub.observeRequest { request = $0 }
         wait(for: [exp], timeout: 1)
+        action(request!)
+            
+        
     }
 }
 
