673cc35ceeea4b31ae9b01b15f7cb5cdbdbd140f
joao <joao.camargo@gmail.com>
2020-12-06 11:15:28 -0300

    add presentation layer

--- CleanApp/Domain/Model/AccountModel.swift
+++ CleanApp/Domain/Model/AccountModel.swift
@@ -11,20 +11,21 @@ public struct AccountModel: Model {
     
     public var accessToken: String
 
-    public init(accessToken: String) {
+    /*public init(accessToken: String) {
+        self.accessToken = accessToken
+    }*/
+    
+    
+    //public var id: String
+    public var name: String
+   // public var email: String
+   // public var password: String
+    
+    public init(/*id: String, */name: String, /*email: String, password: String,*/accessToken: String) {
+        /*self.id = id*/
+        self.name = name
+        /*self.email = email
+        self.password = password*/
         self.accessToken = accessToken
     }
-    
-    /*
-    public var id: String
-    public var name: String
-    public var email: String
-    public var password: String
-    
-    public init(id: String, name: String, email: String, password: String) {
-        self.id = id
-        self.name = name
-        self.email = email
-        self.password = password
-    }*/
 }
---
+++ CleanApp/PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,18 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by joao camargo on 06/12/20.
+//
+
+import XCTest
+@testable import Presentation
+
+class SignUpPresenterTests: XCTestCase {
+
+    func test_() {
+       
+    }
+
+
+}
