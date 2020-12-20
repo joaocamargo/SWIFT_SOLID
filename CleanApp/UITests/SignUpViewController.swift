ded75726620eb9d977c3cdd48485b47925609316
joao <joao.camargo@gmail.com>
2020-12-20 08:38:19 -0300

    allow all projects to run on both iOS and macOS

---
+++ CleanApp/UITests/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by joao camargo on 20/12/20.
+//
+
+import XCTest
+//@testable import UI
+
+class UITests: XCTestCase {
+    func test_() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
